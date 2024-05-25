a650a49d76b572e6af85dec37b5a58a4a5103870
jcdcdev <james@jcdc.dev>
2024-05-25 12:52:38 +0100

    feature: support v14 (WIP)

--- src/Umbraco.Community.FileSystemProviders.B2/Composing/PackageManifest.cs
+++
@@ -1,9 +0,0 @@
-﻿using jcdcdev.Umbraco.Core.Composing;
-
-namespace Umbraco.Community.FileSystemProviders.B2.Composing;
-
-public class PackageManifest : SimplePackageManifest
-{
-    protected override string PackageName => Constants.PackageName;
-    protected override bool AllowPackageTelemetry => true;
-}
